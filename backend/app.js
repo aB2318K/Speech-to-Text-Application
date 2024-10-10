60b0a00492f385ef183b964e7e411eef1da1ab01
Ansh Baral <busybaral@outlook.com>
2024-10-10 20:23:01 +0100

    connect mongodb

---
+++ backend/app.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost:27017', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch((error) => console.log('Error connecting to MongoDB:', error));
