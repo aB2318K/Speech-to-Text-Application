3308434186f65138ed8397dad4d4969b8d493336
Ansh Baral <busybaral@outlook.com>
2024-10-13 10:09:50 +0100

    navigate create

--- speech-to-text/app/dashboard/page.tsx
+++ speech-to-text/app/dashboard/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link"
 export default function Dashboard() {
     return(
          <div>
-            Hello. This is where you will see your saved speeches
+            This is where you will see your saved speeches
+            <br/>
+            <Link href="/create">Create New+</Link>
+            <br/>
+            <Link href="/collaborate">Collaborate</Link>
          </div>
     )
 }
