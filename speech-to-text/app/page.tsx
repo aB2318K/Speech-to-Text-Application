72a65c348483cf0f1247d581d4dc7c8a3ac15843
Ansh Baral <busybaral@outlook.com>
2024-10-17 19:53:57 +0100

    updated css home page

--- speech-to-text/app/page.tsx
+++ speech-to-text/app/page.tsx
@@ -2,26 +2,34 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-teal-50"> 
-      <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-lg">
-        <h2 className="text-3xl font-semibold text-center mb-6 text-teal-700">
-            Speech to Text Application
-        </h2>
-        <div className="space-y-5">
-          <button  className="w-full py-3 px-4 bg-teal-600 text-white rounded-md hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500">
-            <Link className="pr-3" href='/login'>
-              LogIn
-            </Link>
-          </button>
+    <div className="min-h-screen flex items-center justify-center bg-teal-50">
+      <div className="w-full max-w-lg bg-white p-10 rounded-lg shadow-2xl">
+        <h1 className="text-4xl font-semibold text-center text-teal-700 mb-6">
+          Speech to Text Application
+        </h1>
+        <p className="text-center text-teal-600 text-lg mb-8">
+          Effortlessly convert your speech to text with our advanced speech recognition software.
+        </p>
 
-          <button  className="w-full py-3 px-4 bg-teal-600 text-white rounded-md hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500">
-            <Link className="pr-3" href='/signup'>
+        <div className="flex flex-col space-y-5">
+          <Link href="/login">
+            <button
+              className="w-full py-3 px-4 bg-teal-600 text-white rounded-md hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500"
+              >
+              Log In
+            </button>
+          </Link>
+
+          <Link href="/signup">
+            <button
+              className="w-full py-3 px-4 bg-teal-600 text-white rounded-md hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500"
+            >
               Create New Account
-            </Link>
-          </button>
+            </button>
+          </Link>
         </div>
+        
       </div>
-
     </div>
   );
 }
