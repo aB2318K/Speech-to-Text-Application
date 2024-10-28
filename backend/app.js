ec4210a23ebe1518ff0a991f3b948eede621b8df
Learnfinity <support@learnfinity.co.uk>
2024-10-28 20:00:08 +0000

    converted mongoose to require rather than import

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const express = require('express');
